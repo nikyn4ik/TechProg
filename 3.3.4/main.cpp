75e2a860df329e13b752daebc2070c217a024eb8
Nika <92479422+nikyn4ik@users.noreply.github.com>
2023-02-20 23:21:45 +0300

    editing 3.3.4

--- 3.3.4/main.cpp
+++ 3.3.4/main.cpp
@@ -1,11 +1,21 @@
 // main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
-#include <fstream> >//предоставляет возможность чтения и записи файлов в файловой системе
-#include <string>>//строковая библиотека
+#include <fstream>//предоставляет возможность чтения и записи файлов в файловой системе
+#include <string>//строковая библиотека
 //Сообщает компилятору, что нужно включить в программу стандартную библиотеку строковых функций.
 
 using namespace std;
+
+struct Card
+{ //определение структуры для объектов
+    string name;
+    string field1;
+    string field2;
+    string field3;
+    string field4;
+    int trips;
+};
 
 void performTask() {
     char choice;
@@ -39,18 +49,62 @@ void performTask() {
 
 void readFromFile() { // функция читает данные из файла и осуществ. вывод
     ifstream file("cards.txt");
+
     if (file.is_open()) { // если файл успешно открыт
+        Card cards[5];
         string line;
+        int current_line = 0;
+
         while (getline(file, line)) { //чтение и вывод
-            cout << line << endl;
+            int current_struct = 0;
+            string field;
+
+            for (int i = 0, j = line.length() - 2; i < j; i++) {
+                if (line[i] != ' ' && line[i + 1] != ' ') {
+                    field += line[i];
+                }
+                else if (line[i] != ' ' && line[i + 1] == ' ' && line[i + 2] == ' ')
+                {
+                    field += line[i];
+                    switch (current_line)
+                    {
+                    case 0: { cards[current_struct].name = field; break; }
+                    case 1: { cards[current_struct].field1 = field; break; }
+                    case 2: { cards[current_struct].field2 = field; break; }
+                    case 3: { cards[current_struct].field3 = field; break; }
+                    case 4: { cards[current_struct].field4 = field; break; }
+                    case 5: { cards[current_struct].trips = stoi(field); break; }
+                    default: { break; }
+                    }
+
+                    field.clear();
+                    current_struct++;
+                }
+                else if (line[i] == ' ' && line[i - 1] != ' ' && line[i + 1] != ' ')
+                {
+                    field += line[i - 1];
+                    field += line[i];
+                }
+                else {
+                    continue;
+                }
+            }
+
+            current_line++;
         }
+
+        cout << "Which struct you want to print? (0-4)" << endl;
+        int n;
+        cin >> n;
+
+        cout << cards[n].name << endl << cards[n].field1 << endl << cards[n].field2 << endl << cards[n].field3 << endl << cards[n].field4 << endl << cards[n].trips << endl;
+
         file.close(); //закрытие файла
     }
     else {
         cout << "Unable to open file." << endl; // Не удалось открыть файл (метод исключения)
     }
 }
-
 int main() { //осн. функция
     setlocale(LC_ALL, "Russian");
     char choice;
